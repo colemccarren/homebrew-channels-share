04e06f23e5eb218a660e494a833864e11c2c1597
Cole McCarren <45370979+colemccarren@users.noreply.github.com>
2026-09-15 21:32:55 -0400

    Set formula sha256 for v0.2 release tarball

--- Formula/channels-share.rb
+++ Formula/channels-share.rb
@@ -3,7 +3,7 @@ class ChannelsShare < Formula
   homepage "https://github.com/colemccarren/homebrew-channels-share"
   url "https://github.com/colemccarren/homebrew-channels-share/archive/refs/tags/v0.2.tar.gz"
   version "0.2"
-  sha256 "PLACEHOLDER_SHA256_FILLED_AFTER_TAG_PUSH"
+  sha256 "e4f54cd7526a20acbad110b15a01e2ae1eb0e4d28274b04c219b287c85d8ea14"
   license "MIT"
 
   depends_on "bash"
